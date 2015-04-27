18c5637d15da79b73f2fb0156e71f0844a9c7c7f
yxat <yxat@icloud.com>
2015-04-27 14:41:36 +0900

    Initial Commit

---
+++ JunGitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JunGitTest
+//
+//  Created by Jun on 2015. 4. 27..
+//  Copyright (c) 2015년 Codeseeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JunGitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JunGitTest
+//
+//  Created by Jun on 2015. 4. 27..
+//  Copyright (c) 2015년 Codeseeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
